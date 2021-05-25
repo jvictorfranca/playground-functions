ebbda15e8d7ec75c2ad6985d6e402d651b958eea
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-05-25 18:16:44 -0300

    segunda grande arrumaçao de lint

--- src/challenges.js
+++ src/challenges.js
@@ -20,77 +20,79 @@ function calcArea(base, height) {
 //  conta espaços
 function contaEspacos(string) {
   let espacos = [];
-  for (let index = 0 ; index < string.length; index += 1) {
-    if(string[index] === ' ') { espacos.push(index) } 
+  for (let index = 0; index < string.length; index += 1) {
+    if (string[index] === ' ') {
+      espacos.push(index);
+    }
   }
-  return espacos
+  return espacos;
 }
 //  Resolve desafio
 function splitSentence(string) {
   let frase = [];
   let palavra = '';
-  string = ' ' + string + ' ';
-  let espacos = contaEspacos(string);
-  
-  for (index = 0; index < espacos.length-1; index += 1) {
-    palavra = ''
-    for (let index2 = espacos[index]+1; index2 < espacos[index+1]; index2 += 1) {
-      palavra += string[index2]
+  let string2 = ' ' + string + ' ';
+  let espacos = contaEspacos(string2);
+
+  for (let index = 0; index < espacos.length - 1; index += 1) {
+    palavra = '';
+    for (let index2 = espacos[index] + 1; index2 < espacos[index + 1]; index2 += 1) {
+      palavra += string2[index2];
     }
-    frase.push(palavra)
+    frase.push(palavra);
   }
-  return frase
-}  
+  return frase;
+}
 
 // Desafio 4
 function concatName(array) {
   // seu código aqui
   let palavra = '';
-  palavra = array[array.length-1] + ', ' + array[0];
-  return palavra
+  palavra = array[array.length - 1] + ', ' + array[0];
+  return palavra;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  let result = 3 * wins + 1 * ties ;
-  return result
+  let result = 3 * wins + 1 * ties;
+  return result;
 }
 
 // Desafio 6----------------------------------------------------------------------
 //  Acha o maior numero
 function biggest(array) {
-  let maior=array[0]
-  for (let index = 1 ; index < array.length ; index+=1) {
+  let maior = array[0];
+  for (let index = 1; index < array.length; index +=1 ) {
     if (maior < array[index]) {maior = array[index]}
   }
-  return maior
+  return maior;
 }
 //  resolve o desafio
 function highestCount(array) {
   // seu código aqui
   let maior = biggest(array)
   let sum = 0
-  for (let index = 0 ; index < array.length ; index+=1) {
-    if(array[index] === maior) {
+  for (let index = 0; index < array.length; index+=1) {
+    if (array[index] === maior) {
       sum +=1
     } 
   }
-  return sum
+  return sum;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   let cat1_mouse = mouse - cat1;
-  cat1_mouse = Math.abs(cat1_mouse)
+  cat1_mouse = Math.abs(cat1_mouse);
   let cat2_mouse = mouse - cat2;
-  cat2_mouse = Math.abs(cat2_mouse)
+  cat2_mouse = Math.abs(cat2_mouse);
   let answer;
   if (cat1_mouse < cat2_mouse) { answer = 'cat1' }
   else if (cat2_mouse < cat1_mouse) { answer = 'cat2' }
-  else { answer='os gatos trombam e o rato foge' }
-  return answer
+  else { answer = 'os gatos trombam e o rato foge' }
+  return answer;
 }
 
 // Desafio 8 ------------------------------------------------------
@@ -137,17 +139,17 @@ function encode(string) {
     o:4,
     u:5,
   }
-  let codigo = ""
-  let adicao ;
+  let codigo = "";
+  let adicao;
   for (index = 0; index < string.length; index+=1) {
-    adicao=""
+    adicao="";
     for (key in codificador) {
       if (string[index] == key) {adicao+=codificador[key]}
     }
     if (adicao.length===1) {codigo+=adicao}
     else {codigo+=string[index]}
   }
-  return codigo
+  return codigo;
 }
 function decode(string) {
   // Seu código aqui
@@ -159,16 +161,16 @@ function decode(string) {
     5:"u",
   }
   let codigo = ""
-  let adicao ;
+  let adicao;
   for (index = 0; index < string.length; index+=1) {
-    adicao=""
+    adicao="";
     for (key in decodificador) {
-      if (string[index]==key) {adicao+=decodificador[key]}
+      if (string[index]==key) {adicao += decodificador[key]}
     }
-    if (adicao.length===1) {codigo+=adicao}
+    if (adicao.length === 1) {codigo += adicao}
     else {codigo+=string[index]}
   }
-  return codigo
+  return codigo;
   
 }
 
--- src/challenges2.js
+++ src/challenges2.js
@@ -2,21 +2,20 @@
 function techList(array, name) {
   let lista;
   let obj;
-  array = array.sort()
-  if (array.length === 0){
-    lista = "Vazio!"
-  } else {  
+  array = array.sort();
+  if (array.length === 0) {
+    lista = 'Vazio!';
+  } else {
     lista = [];
-    for (let index = 0; index < array.length; index+=1) {
-      obj = { }
-      obj['tech'] = array[index];
-      obj['name'] = name;
-      lista.push(obj)
-      }
+    for (let index = 0; index < array.length; index += 1) {
+      obj = { };
+      obj.tech = array[index];
+      obj.name = name;
+      lista.push(obj);
+    }
   }
-  return lista
-  }
-
+  return lista;
+}
 
 // Desafio 11
 function generatePhoneNumber() {
