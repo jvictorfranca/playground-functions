5b1e8df28aa39f7e80b535b707e20e473419d31f
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-05-25 15:16:41 -0300

    resolve o terceiro desafio, e muda o gitignore para adicionar pasta de testes

--- src/challenges.js
+++ src/challenges.js
@@ -10,10 +10,31 @@ function calcArea(base, height) {
 return (base*height)/2
 }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+/////////////////////////////////////// Desafio 3 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//conta espaços
+function contaEspacos(string){
+  let espacos=[]
+  for(let index=0; index<string.length; index+=1){
+    if(string[index]===' '){espacos.push(index)} 
+  }
+return espacos
 }
+//resolve desafio
+function splitSentence(string) {
+  let frase=[]
+  let palavra=''
+  string = ' ' + string + ' '
+  let espacos = contaEspacos(string)
+  
+  for(index=0; index<espacos.length-1; index+=1){
+    palavra = ''
+    for(let index2= espacos[index]+1; index2<espacos[index+1]; index2+=1){
+      palavra += string[index2]
+    }
+    frase.push(palavra)
+  }
+return frase
+}  
 
 // Desafio 4
 function concatName() {
