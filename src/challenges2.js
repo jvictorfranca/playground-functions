36fa4c6e92716d51c1c73cf585ae827c819fce43
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-05-26 19:46:47 -0300

    resolve todos os desafios

--- src/challenges2.js
+++ src/challenges2.js
@@ -146,8 +146,32 @@ return answer
 }
 
 // Desafio 13
-function hydrate() {
+//Fonte: https://www.youtube.com/watch?v=pfkkdzeyx6U
+
+function contaNumeros(string){
+  let reg = /\d+/g;
+  let result = string.match(reg)
+  return result
+}
+
+function soma(array){
+  let somado = 0
+  for (let index = 0; index < array.length; index += 1){
+    somado = somado + parseInt(array[index])
+    
+  }
+  return somado
+}
+
+
+function hydrate(string) {
   // seu código aqui
+  let agua = soma(contaNumeros(string))
+  let answer;
+  if (agua > 1){answer = agua + " copos de água"}
+  else {answer = agua + " copo de água"}
+  return answer
+  
 }
 
 module.exports = {
